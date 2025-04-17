e7aacd6d376773c7cc3f7b9897901dd867478f7f
alnaimi-github <alnymybdalwas1@gmail.com>
2025-04-17 17:10:00 +0300

    Add RabbitMQ producer-consumer functionality
    Added RabbitMQ.Client NuGet package (v7.1.2) to both projects.
    Implemented a producer in `DirectExchange.Producer` to publish
    messages to a direct exchange (`amq.direct`) with a routing key.
    Implemented a consumer in `DirectExchange.Consumer` to process
    messages from the `qu-notification` queue. Removed default
    template code from both projects.

--- DirectExchange.Consumer/Program.cs
+++ DirectExchange.Consumer/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+await using var connection = await factory.CreateConnectionAsync();
+await using var channel = await connection.CreateChannelAsync();
+
+// consumer
+var consumer = new AsyncEventingBasicConsumer(channel);
+
+consumer.ReceivedAsync += async (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = System.Text.Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+    await Task.Yield();
+};
+
+await channel.BasicConsumeAsync(queue: "qu-notification",
+    autoAck: true,
+    consumer: consumer);
+
--- RabbitMQ/Program.cs
+++ RabbitMQ/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+await using var connection = await factory.CreateConnectionAsync();
+await using var channel = await connection.CreateChannelAsync();
+
+//await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false,
+//    arguments: null);
+
+const string message = "Hello World!";
+var body = Encoding.UTF8.GetBytes(message);
+
+await channel.BasicPublishAsync(exchange: "amq.direct", routingKey: "key-notification", body: body);
+
+Console.WriteLine($" [x] Sent {message}");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
